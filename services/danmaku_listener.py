cb92e0a7c145d04662d5c393f06d0c4bd3111178
星空凌 <2374416274@qq.com>
2026-06-09 19:09:50 +0800

    fix: 导入缺失的get_beijing_now

--- services/danmaku_listener.py
+++ services/danmaku_listener.py
@@ -9,6 +9,7 @@ import blivedm
 import blivedm.models.web as web_models
 
 from config import ROOM_ID, SESSDATA, BUVID3, BILI_JCT
+from db.models import get_beijing_now
 from services.auth_service import (
     get_active_auth_session,
     get_cached_code,
